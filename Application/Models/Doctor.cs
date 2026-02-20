93eb9e4989f508d81d5b86f3afc6156a8bd0394d
wenz <wenz@spengergasse.at>
2026-02-20 13:51:58 +0100

    feat: doctor

--- Application/Models/Doctor.cs
+++ Application/Models/Doctor.cs
@@ -2,5 +2,23 @@ namespace Application.Models;
 
 public class Doctor
 {
+    // PK
+    public int Id { get; set; }
     
+    public string FirstName { get; set; }
+    
+    public string LastName { get; set; }
+    
+    public string Email { get; set; }
+    
+    // --- EF Ctor ---
+    protected Doctor() { }
+    
+    // --- Business Ctor ---
+    public Doctor(string firstName, string lastName, string email)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+    }
 }
