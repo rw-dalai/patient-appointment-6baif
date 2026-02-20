784b3248c7f9116f1ed973df50433bfc656a7329
wenz <wenz@spengergasse.at>
2026-02-20 13:51:43 +0100

    feat: patient

--- Application/Models/Patient.cs
+++ Application/Models/Patient.cs
@@ -2,5 +2,28 @@ namespace Application.Models;
 
 public class Patient
 {
+    // PK
+    public int Id { get; set; }
     
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public InsuranceNumber InsuranceNumber { get; set; }
+
+    public PhoneNumber PhoneNumber { get; set; }
+        
+    // --- EF Ctor ---
+    protected Patient() { }
+    
+    // --- Business Ctor ---
+    public Patient(string firstName, string lastName,
+        InsuranceNumber insuranceNumber, PhoneNumber phoneNumber)
+    {
+        
+        FirstName = firstName;
+        LastName = lastName;
+        InsuranceNumber = insuranceNumber;
+        PhoneNumber = phoneNumber;
+    }
 }
